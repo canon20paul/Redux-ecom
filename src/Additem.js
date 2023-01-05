a4922faa972f8a65346ba818717899d66918e38f
canon20paul <canon20paul@gmail.com>
2023-01-05 07:45:43 +0300

    Components created

---
+++ src/Additem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Additems() {
+  return (
+      <div>This is Component Additems</div>
+  )
+}
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Items from './Items';
+import Additem from './Additem';
+import Cart from './Cart';
+import Footer from './Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar/>
+     <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Items />} />
+          <Route path='/additem' element={<Additem />} />
+          <Route path='/cart' element={<Cart />} />
+      </Routes>
+
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
