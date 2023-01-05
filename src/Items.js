4f61c051f9b8d9fde6c766a623ea54363f9064c9
canon20paul <canon20paul@gmail.com>
2023-01-05 18:24:59 +0300

    Filter function added

--- src/Items.js
+++ src/Items.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useSelector } from 'react-redux'
 import Item from './Item'
 
 export default function Items() { 
   const itemsobj = useSelector(store=>store.itemsReducer)
-  const itemslist = itemsobj.items
-
+  const [itemslist , setitemslist]=useState( itemsobj.items)
+  const [searchkey , setsearchkey]=useState('')
+  function filteritems(){
+    const duplicatelist = itemsobj.items
+    const filteredlist = duplicatelist.filter(item=> item.itemName.toLowerCase().includes(searchkey.toLowerCase()))
+    setitemslist(filteredlist)
+  }
   const itemsdata = itemslist.map((item)=>{
     return <div className='col-md-4'>
       <Item item={item}/>
@@ -13,7 +18,9 @@ export default function Items() {
 
   })
   return (
-      <div>
+      <div className='container'>
+      <input type="text" className='form-control mb-5' placeholder='Search Items' value={searchkey} onChange={(e)=>{
+      setsearchkey(e.target.value)}} onKeyUp={filteritems}/>
            <div className='row'>
           {itemsdata}
       </div>
