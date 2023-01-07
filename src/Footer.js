a9f2843e94573e1eda053ee581a12778a3f82784
canon20paul <canon20paul@gmail.com>
2023-01-07 15:57:38 +0300

    Updating Readme

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-      <div style={{backgroundColor:'blue',color:'white'}}>
+      <div style={{backgroundColor:'blue',color:'white', marginBottom:'0'}}>
           <p>Designed and Developed by</p>    
           <p>Paul Malongo</p>
 
